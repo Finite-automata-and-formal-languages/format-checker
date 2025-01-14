c2457f352f56267da9740e9a8924fea9c99851d6
Naïm Favier <n@monade.li>
2025-01-14 14:34:41 +0100

    Add a validator for context-free grammars

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -2,8 +2,10 @@ use dandy::dfa::Dfa;
 use dandy::dfa::parse::DfaParseError;
 use dandy::nfa::Nfa;
 use dandy::nfa::parse::NfaParseError;
+use dandy::grammar::Grammar;
+use dandy::grammar::parse::GrammarParseError;
 use dandy::regex::Regex;
-use dandy::parser::{dfa, nfa, regex};
+use dandy::parser::{dfa, nfa, regex, grammar};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 trait AcceptsGraphemes {
@@ -44,16 +46,12 @@ impl AcceptsGraphemes for Regex {
 
 #[wasm_bindgen]
 pub fn check_dfa(test: &str, input: &str) -> Result<(), String> {
-    let dfa: Dfa = dfa(input)
-        .map_err(|e| format!("Error parsing DFA: {e:?}"))?
+    let _dfa: Dfa = dfa(input)
+        .map_err(|e| format!("Error parsing DFA: {e}"))?
         .try_into()
         .map_err(|e: DfaParseError| e.to_string())?;
     match test {
         "valid" => (),
-        "assignment-2-2-1" => {
-            dfa.should_accept("abc")?;
-            dfa.should_reject("bba")?;
-        },
         _ => return Err(format!("test {test} not implemented"))
     }
     Ok(())
@@ -62,12 +60,12 @@ pub fn check_dfa(test: &str, input: &str) -> Result<(), String> {
 #[wasm_bindgen]
 pub fn check_nfa(test: &str, input: &str) -> Result<(), String> {
     let nfa: Nfa = nfa(input)
-        .map_err(|e| format!("Error parsing NFA: {e:?}"))?
+        .map_err(|e| format!("Error parsing NFA: {e}"))?
         .try_into()
         .map_err(|e: NfaParseError| e.to_string())?;
     match test {
         "valid" => (),
-        "assignment-2-3" => {
+        "assignment-2-3-1" => {
             nfa.should_accept("abaab")?;
             nfa.should_accept("babbabb")?;
             nfa.should_reject("ababab")?;
@@ -80,7 +78,7 @@ pub fn check_nfa(test: &str, input: &str) -> Result<(), String> {
 #[wasm_bindgen]
 pub fn check_regex(test: &str, input: &str) -> Result<(), String> {
     let regex: Regex = regex(input)
-        .map_err(|e| format!("Error parsing regex: {e:?}"))?;
+        .map_err(|e| format!("Error parsing regex: {e}"))?;
     let nfa = regex.to_nfa();
     match test {
         "valid" => (),
@@ -90,6 +88,19 @@ pub fn check_regex(test: &str, input: &str) -> Result<(), String> {
             nfa.should_reject("11")?;
             nfa.should_reject("1001")?;
         }
+        _ => return Err(format!("test {test} not implemented"))
+    }
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn check_grammar(test: &str, input: &str) -> Result<(), String> {
+    let _grammar: Grammar = grammar(input)
+        .map_err(|e| format!("Error parsing CFG: {e}"))?
+        .try_into()
+        .map_err(|e: GrammarParseError| e.to_string())?;
+    match test {
+        "valid" => (),
         _ => return Err(format!("test {test} not implemented"))
     }
     Ok(())
