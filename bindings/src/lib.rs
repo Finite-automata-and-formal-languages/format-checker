b250478998f8b3608177bf7582c2f4a560bd290b
JonasHoefer <hoferj@chalmers.se>
2025-01-15 17:31:21 +0100

    Use equivalence check in front end

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,33 +1,61 @@
-use dandy::dfa::Dfa;
 use dandy::dfa::parse::DfaParseError;
-use dandy::nfa::Nfa;
-use dandy::nfa::parse::NfaParseError;
-use dandy::grammar::Grammar;
+use dandy::dfa::Dfa;
 use dandy::grammar::parse::GrammarParseError;
+use dandy::grammar::Grammar;
+use dandy::nfa::parse::NfaParseError;
+use dandy::nfa::Nfa;
+use dandy::parser::{dfa, grammar, nfa, regex};
 use dandy::regex::Regex;
-use dandy::parser::{dfa, nfa, regex, grammar};
 use std::collections::HashMap;
 use std::sync::LazyLock;
-use std::sync::Mutex;
 use wasm_bindgen::prelude::wasm_bindgen;
 
+/// Expected format is
+/// ```yaml
+/// assignment-3-2: |
+///   the code for
+///   the automata
+/// ...
+/// ```
+/// The contents of the file are supposed to be stored as a GitHub secret,
+/// and are written to this file at deployment.
 static SOLUTION_STR: &str = include_str!("../assignments.yaml");
 
-static SOLUTION: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
-    serde_yaml::from_str(&SOLUTION_STR).unwrap()
-});
+static SOLUTION: LazyLock<HashMap<String, String>> =
+    LazyLock::new(|| serde_yaml::from_str(&SOLUTION_STR).unwrap());
 
 #[wasm_bindgen]
 pub fn check_dfa(test: &str, input: &str) -> Result<(), String> {
-    let _dfa: Dfa = dfa(input)
+    let proposed_dfa: Dfa = dfa(input)
         .map_err(|e| format!("Error parsing DFA: {e}"))?
         .try_into()
         .map_err(|e: DfaParseError| e.to_string())?;
-    match test {
-        "valid" => (),
-        _ => return Err(format!("test {test} not implemented"))
+
+    if test == "valid" {
+        return Ok(());
     }
-    Ok(())
+
+    let solution_dfa_src = SOLUTION
+        .get(test)
+        .ok_or(format!("No solution for {}", test))?;
+
+    let solution_dfa: Dfa = dfa(solution_dfa_src)
+        .map_err(|e| format!("Error parsing solution DFA: {e}"))?
+        .try_into()
+        .map_err(|e: DfaParseError| e.to_string())?;
+
+    if let Some(sep) = solution_dfa.separable_from(&proposed_dfa) {
+        let e = match sep {
+            None => "The provided NFA has the wrong alphabet!".to_string(),
+            Some(w) => format!(
+                "The solution and the provided NFA disagree on the word {}",
+                w
+            ),
+        };
+        Err(e)
+    } else {
+        Ok(())
+    }
 }
 
 #[wasm_bindgen]
@@ -36,33 +64,64 @@ pub fn check_nfa(test: &str, input: &str) -> Result<(), String> {
         .map_err(|e| format!("Error parsing NFA: {e}"))?
         .try_into()
         .map_err(|e: NfaParseError| e.to_string())?;
-    match test {
-        "valid" => (),
-        s => {
-            let solution_nfa: Nfa = nfa(s)
-                .map_err(|e| format!("Error parsing solution NFA: {e}"))?
-                .try_into()
-                .map_err(|e: NfaParseError| e.to_string())?;
-            // let x = solution_nfa.separable_from(&proposed_nfa);
-        }
-        _ => return Err(format!("test {test} not implemented"))
+
+    if test == "valid" {
+        return Ok(());
     }
-    Ok(())
+
+    let solution_nfa_src = SOLUTION
+        .get(test)
+        .ok_or(format!("No solution for {}", test))?;
+
+    let solution_nfa: Nfa = nfa(solution_nfa_src)
+        .map_err(|e| format!("Error parsing solution NFA: {e}"))?
+        .try_into()
+        .map_err(|e: NfaParseError| e.to_string())?;
+
+    if let Some(sep) = solution_nfa.separable_from(&proposed_nfa) {
+        let e = match sep {
+            None => "The provided NFA has the wrong alphabet!".to_string(),
+            Some(w) => format!(
+                "The solution and the provided NFA disagree on the word {}",
+                w
+            ),
+        };
+        Err(e)
+    } else {
+        Ok(())
+    }
 }
 
 #[wasm_bindgen]
 pub fn check_regex(test: &str, input: &str) -> Result<(), String> {
-    let regex: Regex = regex(input)
-        .map_err(|e| format!("Error parsing regex: {e}"))?;
-    let nfa = regex.to_nfa();
-    match test {
-        "valid" => (),
-        "assignment-3-2" => {
-            
-        }
-        _ => return Err(format!("test {test} not implemented"))
+    let proposed_regex: Regex = regex(input).map_err(|e| format!("Error parsing regex: {e}"))?;
+
+    if test == "valid" {
+        return Ok(());
     }
-    Ok(())
+
+    let solution_regex_src = SOLUTION
+        .get(test)
+        .ok_or(format!("No solution for {}", test))?;
+
+    let solution_regex: Regex =
+        regex(solution_regex_src).map_err(|e| format!("Error parsing solution Regex: {e}"))?;
+
+    if let Some(sep) = solution_regex
+        .to_nfa()
+        .separable_from(&proposed_regex.to_nfa())
+    {
+        let e = match sep {
+            None => "The provided Regex has the wrong alphabet!".to_string(),
+            Some(w) => format!(
+                "The solution and the provided Regex disagree on the word {}",
+                w
+            ),
+        };
+        Err(e)
+    } else {
+        Ok(())
+    }
 }
 
 #[wasm_bindgen]
@@ -73,7 +132,7 @@ pub fn check_grammar(test: &str, input: &str) -> Result<(), String> {
         .map_err(|e: GrammarParseError| e.to_string())?;
     match test {
         "valid" => (),
-        _ => return Err(format!("test {test} not implemented"))
+        _ => return Err(format!("test {test} not implemented")),
     }
     Ok(())
 }
