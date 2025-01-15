d34ef9d6682aa2254c67a740d5d1928b37de5056
Jonas Hoefer <hoferj@chalmers.se>
2025-01-15 11:39:39 +0100

    Start working on front end for equivalence checking

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -6,43 +6,16 @@ use dandy::grammar::Grammar;
 use dandy::grammar::parse::GrammarParseError;
 use dandy::regex::Regex;
 use dandy::parser::{dfa, nfa, regex, grammar};
+use std::collections::HashMap;
+use std::sync::LazyLock;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::wasm_bindgen;
 
-trait AcceptsGraphemes {
-    fn accepts_graphemes(&self, word: &str) -> bool;
+static SOLUTION_STR: &str = include_str!("../assignments.yaml");
 
-    fn should_accept(&self, word: &str) -> Result<(), String> {
-        if !self.accepts_graphemes(word) {
-            return Err(format!("The word `{word}` should be accepted, but it is rejected."))
-        }
-        Ok(())
-    }
-
-    fn should_reject(&self, word: &str) -> Result<(), String> {
-        if self.accepts_graphemes(word) {
-            return Err(format!("The word `{word}` should be rejected, but it is accepted."))
-        }
-        Ok(())
-    }
-}
-
-impl AcceptsGraphemes for Dfa {
-    fn accepts_graphemes(&self, word: &str) -> bool {
-        self.accepts_graphemes(word)
-    }
-}
-
-impl AcceptsGraphemes for Nfa {
-    fn accepts_graphemes(&self, word: &str) -> bool {
-        self.accepts_graphemes(word)
-    }
-}
-
-impl AcceptsGraphemes for Regex {
-    fn accepts_graphemes(&self, word: &str) -> bool {
-        self.clone().to_nfa().accepts_graphemes(word)
-    }
-}
+static SOLUTION: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
+    serde_yaml::from_str(&SOLUTION_STR).unwrap()
+});
 
 #[wasm_bindgen]
 pub fn check_dfa(test: &str, input: &str) -> Result<(), String> {
@@ -59,16 +32,18 @@ pub fn check_dfa(test: &str, input: &str) -> Result<(), String> {
 
 #[wasm_bindgen]
 pub fn check_nfa(test: &str, input: &str) -> Result<(), String> {
-    let nfa: Nfa = nfa(input)
+    let proposed_nfa: Nfa = nfa(input)
         .map_err(|e| format!("Error parsing NFA: {e}"))?
         .try_into()
         .map_err(|e: NfaParseError| e.to_string())?;
     match test {
         "valid" => (),
-        "assignment-2-3-1" => {
-            nfa.should_accept("abaab")?;
-            nfa.should_accept("babbabb")?;
-            nfa.should_reject("ababab")?;
+        s => {
+            let solution_nfa: Nfa = nfa(s)
+                .map_err(|e| format!("Error parsing solution NFA: {e}"))?
+                .try_into()
+                .map_err(|e: NfaParseError| e.to_string())?;
+            // let x = solution_nfa.separable_from(&proposed_nfa);
         }
         _ => return Err(format!("test {test} not implemented"))
     }
@@ -83,10 +58,7 @@ pub fn check_regex(test: &str, input: &str) -> Result<(), String> {
     match test {
         "valid" => (),
         "assignment-3-2" => {
-            nfa.should_accept("101")?;
-            nfa.should_accept("10101")?;
-            nfa.should_reject("11")?;
-            nfa.should_reject("1001")?;
+            
         }
         _ => return Err(format!("test {test} not implemented"))
     }
